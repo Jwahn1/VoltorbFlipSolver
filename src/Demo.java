0f17c554a5a57a7c12440333c59ff91bcc3f1f43
Jwahn1 <javierwith@gmail.com>
2026-03-05 15:43:40 -0400

    Added terminal menu

--- src/Demo.java
+++ src/Demo.java
@@ -6,7 +6,24 @@ public class Demo {
         Board board = new Board();
         board.setRowConstraints();
         board.setColumnConstraints();
-        board.printBoard();
+
+
+        Scanner scan = new Scanner(System.in);
+        boolean playing = true;
+        int gameChoice;
+        while(playing){
+            board.printBoard();
+            System.out.print("\n\nChoose option - \n1.Flip Card\n2.End Game\n");
+            gameChoice = scan.nextInt();
+
+            switch(gameChoice){
+                case 1:
+                    break;
+                case 2:
+                    playing = false;
+                    break;
+            }
+        }
 
     }
 }
